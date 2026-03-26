0023d96929912dd7d8287e26d9156dded17fc704
ToastedTea <bruh1234r@gmail.com>
2026-03-26 08:33:13 -0500

    Update pickle.lua

--- pickle.lua
+++ pickle.lua
@@ -9,19 +9,37 @@ workspace.ChildAdded:Connect(function(chil)
 
 		local ticklepickle = true		
 		local thingy
+		local secondary = nil
+		local primary = nil
+
+		local done = false
 		thingy = game:GetService("RunService").RenderStepped:Connect(function()
 			if ticklepickle == false then return end
+				for i,v in game:GetService("Workspace")[game.Players.LocalPlayer.Name .. " Aircraft"]:GetDescendants() do
+					if v:IsA("BasePart") and v.Color == _G.SECONDARYCOLOR then secondary = v end
+				end
+
+			end
 			for i,v in game:GetService("Workspace")[game.Players.LocalPlayer.Name .. " Aircraft"]:GetDescendants() do
 
 				if v:IsA("BasePart") and v.Color == _G.TARGETCOLOR then
 					ticklepickle = false
-					loadstring(game:HttpGet("https://raw.githubusercontent.com/ToastedTea/gya/refs/heads/main/gyaaa.lua"))()
+					primary = v
+					loadstring(game:HttpGet("https://raw.githubusercontent.com/ToastedTea/gya/refs/heads/main/gyaaa.lua"))()(v)
+					done = true
 					thingy:Disconnect()
-
 				end
 
 			end
+			done = true
 		end)
+		repeat task.wait() until done == true
+			
+		if primary ~= nil and secondary ~= nil then
+			repeat task.wait() until primary.Parent == nil
+			loadstring(game:HttpGet("https://raw.githubusercontent.com/ToastedTea/gya/refs/heads/main/gyaaa.lua"))()(secondary)
+		end
+			
 	end)
 
 
