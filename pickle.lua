791aa416bdeb364072d2fe9db8cf5e6b25e7d6e7
ToastedTea <bruh1234r@gmail.com>
2026-03-26 11:48:21 -0500

    Update pickle.lua

--- pickle.lua
+++ pickle.lua
@@ -1,4 +1,3 @@
-
 workspace.ChildAdded:Connect(function(chil)
 	task.spawn(function()
 		local air = game:GetService("Workspace"):WaitForChild(game.Players.LocalPlayer.Name .. " Aircraft",2)
@@ -15,32 +14,36 @@ workspace.ChildAdded:Connect(function(chil)
 		local done = false
 		thingy = game:GetService("RunService").RenderStepped:Connect(function()
 			if ticklepickle == false then return end
+			if not game.Workspace:FindFirstChild(game.Players.LocalPlayer.Name.. " Aircraft") then return end
 			for i,v in game:GetService("Workspace")[game.Players.LocalPlayer.Name .. " Aircraft"]:GetDescendants() do
-				if v:IsA("BasePart") and v.Color == _G.SECONDARYCOLOR then secondary = v end
+				if v:IsA("BasePart") and v.Color == _G.SECONDARYCOLOR then secondary = v print("found secnd") end
 			end
-			
+
 			for i,v in game:GetService("Workspace")[game.Players.LocalPlayer.Name .. " Aircraft"]:GetDescendants() do
 
 				if v:IsA("BasePart") and v.Color == _G.TARGETCOLOR then
 					ticklepickle = false
 					primary = v
-					loadstring(game:HttpGet("https://raw.githubusercontent.com/ToastedTea/gya/refs/heads/main/gyaaa.lua"))()(v)
+					local fa = loadstring(game:HttpGet("https://raw.githubusercontent.com/ToastedTea/gya/refs/heads/main/gyaaa.lua"))
+					fa(v)
 					done = true
 					thingy:Disconnect()
 				end
 
 			end
-			done = true
 		end)
-		
-		
-		repeat task.wait() until done == true
 
+
+		repeat task.wait() until done == true and primary ~= nil
+		if _G.EXPECTSECOND then repeat task.wait() until secondary ~= nil end
+		print(primary.Name)
+		print(secondary.Name)
 		if primary ~= nil and secondary ~= nil then
-			repeat task.wait() until primary.Parent == nil
-			loadstring(game:HttpGet("https://raw.githubusercontent.com/ToastedTea/gya/refs/heads/main/gyaaa.lua"))()(secondary)
+			repeat task.wait() until primary.Parent == nil or primary == nil or primary:HasTag("byebye")
+			local fa = loadstring(game:HttpGet("https://raw.githubusercontent.com/ToastedTea/gya/refs/heads/main/gyaaa.lua"))
+	
+			print("Ya")
+			fa(secondary)
 		end
 	end)
-	
-
 end)
